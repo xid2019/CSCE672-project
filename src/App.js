c4879c630aebc93672b4e86ea2b2b91ae63b87b8
Jincheng Li <jinchengli97@gmail.com>
2022-10-31 23:40:22 -0500

    complete main features and partially styled

--- src/App.js
+++ src/App.js
@@ -24,114 +24,6 @@ function SomeoneIsTyping() {
   );
 }
 
-function AddDescription(props) {
-  const [description, setDescription] = useState("");
-  const [myPresence, updateMyPresence] = useMyPresence();
-  const addDescription = useMutation(({ storage }, description) => {
-    const todo = storage.get("todos").get(props.index)
-    todo.description = description
-    storage.get("todos").set(props.index, todo)
-  }, []);
-  return (
-    <input
-        type="text"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-          updateMyPresence({ isTyping: true });
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            updateMyPresence({ isTyping: false });
-            addDescription(description)
-            setDescription("");
-          }
-        }}
-        onBlur={() => updateMyPresence({ isTyping: false })}
-      />
-  )
-}
-
-function AddGroupChat(props) {
-  const [groupChat, setGroupChat] = useState("");
-  const [myPresence, updateMyPresence] = useMyPresence();
-  const addGroupChat = useMutation(({ storage }, groupChat) => {
-    const todo = storage.get("todos").get(props.index)
-    if (!todo.groupChat) {
-      todo.groupChat = groupChat
-    } else {
-      todo.groupChat = todo.groupChat + '\n' + groupChat
-    }
-    
-    storage.get("todos").set(props.index, todo)
-  }, []);
-  return (
-    <input
-        type="text"
-        placeholder="Group chat"
-        value={groupChat}
-        onChange={(e) => {
-          setGroupChat(e.target.value);
-          updateMyPresence({ isTyping: true });
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            updateMyPresence({ isTyping: false });
-            setGroupChat(e.target.value);
-            addGroupChat(groupChat)
-            setGroupChat("");
-          }
-        }}
-        onBlur={() => updateMyPresence({ isTyping: false })}
-      />
-  )
-}
-
-function AddType(props) {
-  const addType = useMutation(({ storage }, type) => {
-    const todo = storage.get("todos").get(props.index)
-    todo.type = type
-    storage.get("todos").set(props.index, todo)
-  }, []);
-  return (
-    <select 
-      name="type" 
-      id="type"
-      onChange={(e) => {
-        addType(e.target.value)
-      }}
-    >
-      <option value="develop">develop</option>
-      <option value="debug">debug</option>
-      <option value="data processing">data processing</option>
-      <option value="devops">devops</option>
-    </select>
-  )
-}
-
-function AddDeadline(props) {
-  const addDeadline = useMutation(({ storage }, deadline) => {
-    const todo = storage.get("todos").get(props.index)
-    todo.deadline = deadline
-    storage.get("todos").set(props.index, todo)
-  }, []);
-  return (
-    <input 
-      type="date" 
-      id="deadline" 
-      name="deadline"
-      value="2018-07-22"
-      min="2018-01-01" 
-      max="2050-12-31"
-      onChange={(e) => {
-          addDeadline(e.target.value)
-      }}
-    >
-    </input>
-  )
-}
-
 function Room() {
   const [draft, setDraft] = useState("");
   const [myPresence, updateMyPresence] = useMyPresence();
@@ -139,10 +31,6 @@ function Room() {
   console.log(todos);
   const addTodo = useMutation(({ storage }, text) => {
     storage.get("todos").push({ text })
-  }, []);
-
-  const deleteTodo = useMutation(({ storage }, index) => {
-    storage.get("todos").delete(index);
   }, []);
 
   return (
@@ -176,23 +64,6 @@ function Room() {
         }
         return (
           <TodoCard todo={todo} key={index} index={index}/>
-          // <div key={index} className="todo_container">
-          //   <div className="todo">{todo.text}</div>
-          //   <div className="description">{todo.description}</div>
-          //   <div className="type">{todo.type}</div>
-          //   <div className="deadline">{todo.deadline}</div>
-          //   <div className="groupChat">{todo.groupChat}</div>
-          //   <AddDescription index={index}/>
-          //   <AddType index={index}/>
-          //   <AddDeadline index={index}/>
-          //   <AddGroupChat index={index}/>
-          //   <button
-          //     className="delete_button"
-          //     onClick={() => deleteTodo(index)}
-          //   >
-          //     ✕
-          //   </button>
-          // </div>
         );
       })}
     </div>
--- src/TodoCard.js
+++ src/TodoCard.js
@@ -4,24 +4,24 @@ import { RoomProvider, useOthers, useUpdateMyPresence, useMyPresence, useStorage
 import { LiveList } from "@liveblocks/client";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
+import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import DeleteIcon from '@mui/icons-material/Delete';
 import DescriptionIcon from "@mui/icons-material/Description";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import CategoryIcon from '@mui/icons-material/Category';
+import PersonIcon from '@mui/icons-material/Person';
 import TextField from "@mui/material/TextField";
+import Select from '@mui/material/Select';
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import MenuItem from '@mui/material/MenuItem';
 import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
@@ -37,11 +37,15 @@ const ExpandMore = styled((props) => {
 }));
 
 const TodoCard = (props) => {
-    const {text, description, type, deadline, groupchat} = props.todo;
+    const {text, description, type, deadline, person, status, groupchat} = props.todo;
     const index = props.index;
     const [expanded, setExpanded] = useState(false);
     const [ddl, setDdl] = useState(dayjs(deadline));
     const [todoText, setTodoText] = useState("");
+    const [todoDescription, setTodoDescription] = useState("");
+    const [todoType, setTodoType] = useState(type);
+    const [todoPerson, setTodoPerson] = useState(person);
+    const [todoStatus, setTodoStatus] = useState(status);
     const [myPresence, updateMyPresence] = useMyPresence();
     const modifyTodoText = useMutation(({storage}, idx, draft) => {
         let todo = storage.get("todos").get(idx)
@@ -57,6 +61,38 @@ const TodoCard = (props) => {
         setDdl(draft)
     }, [])
 
+    const modifyTodoDescription = useMutation(({storage}, idx, draft) => {
+        let todo = storage.get("todos").get(idx)
+        todo.description = draft
+        storage.get("todos").set(idx, todo)
+        setTodoDescription("")
+    }, [])
+
+    const modifyTodoType = useMutation(({storage}, idx, draft) => {
+        let todo = storage.get("todos").get(idx)
+        todo.type = draft
+        storage.get("todos").set(idx, todo)
+        setTodoType(draft)
+    }, [])
+
+    const modifyTodoPerson = useMutation(({storage}, idx, draft) => {
+        let todo = storage.get("todos").get(idx)
+        todo.person = draft
+        storage.get("todos").set(idx, todo)
+        setTodoPerson(draft)
+    }, [])
+
+    const modifyTodoStatus = useMutation(({storage}, idx, draft) => {
+        let todo = storage.get("todos").get(idx)
+        todo.status = draft
+        storage.get("todos").set(idx, todo)
+        setTodoStatus(draft)
+    }, [])
+
+    const deleteTodo = useMutation(({storage}, idx) => {
+        storage.get("todos").delete(idx);
+    }, [])
+
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
@@ -64,7 +100,7 @@ const TodoCard = (props) => {
     return (
         <Card sx={{ maxWidth: 345, bgcolor: "#caf0f8", m:1, p:1 }}>
             <CardContent>
-                <input type="text" placeholder="Change this Todo text" value={todoText}
+                <input type="text" placeholder="Edit Todo title here" value={todoText}
                     onChange={(e) => {
                         setTodoText(e.target.value);
                         updateMyPresence({ isTyping: true });
@@ -87,15 +123,56 @@ const TodoCard = (props) => {
                 <Typography>Deadline</Typography>
                 </IconButton>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateTimePicker
-                    value={ddl}
-                    onChange={(newValue) => {
-                        modifyDdl(index, newValue)
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                />
+                    <DateTimePicker
+                        value={ddl}
+                        onChange={(newValue) => {
+                            modifyDdl(index, newValue)
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
                 </LocalizationProvider>
             </CardActions>
+            <CardActions sx={{ justifyContent: "space-between" }} disableSpacing>
+                <IconButton>
+                <CategoryIcon />
+                <Typography>Task Type</Typography>
+                </IconButton>
+                <Select id="todoType" value={todoType} onChange={(event) => {modifyTodoType(index, event.target.value)}}>
+                    <MenuItem value="Features">Features</MenuItem>
+                    <MenuItem value="Testing">Testing</MenuItem>
+                    <MenuItem value="Styling">Styling</MenuItem>
+                    <MenuItem value="Devops">Devops</MenuItem>
+                    <MenuItem value="Bug fix">Bug fix</MenuItem>
+                    <MenuItem value="Others">Others</MenuItem>
+                </Select>
+            </CardActions>
+            <CardActions sx={{ justifyContent: "space-between" }} disableSpacing>
+                <IconButton>
+                <PersonIcon />
+                <Typography>Assigned To</Typography>
+                </IconButton>
+                <Select id="todoPerson" value={todoPerson} onChange={(event) => {modifyTodoPerson(index, event.target.value)}}>
+                    <MenuItem value="Nobody">Nobody</MenuItem>
+                    <MenuItem value="Fengyi Wang">Fengyi Wang</MenuItem>
+                    <MenuItem value="Jincheng Li">Jincheng Li</MenuItem>
+                    <MenuItem value="Sibo Min">Sibo Min</MenuItem>
+                    <MenuItem value="Xiaomu Dong">Xiaomu Dong</MenuItem>
+                    <MenuItem value="Zihan Wang">Zihan Wang</MenuItem>
+                </Select>
+            </CardActions>
+            <CardActions sx={{ justifyContent: "space-between" }} disableSpacing>
+                <IconButton>
+                <DonutLargeIcon />
+                <Typography>Progress</Typography>
+                </IconButton>
+                <Select id="todoStatus" value={todoStatus} onChange={(event) => {modifyTodoStatus(index, event.target.value)}}>
+                    <MenuItem value="In Progress">In Progress</MenuItem>
+                    <MenuItem value="Testing">Testing</MenuItem>
+                    <MenuItem value="Completed">Completed</MenuItem>
+                    <MenuItem value="Canceled">Canceled</MenuItem>
+                </Select>
+            </CardActions>
+            
             <CardActions disableSpacing>
                 <IconButton>
                 <DescriptionIcon />
@@ -112,11 +189,33 @@ const TodoCard = (props) => {
             </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
+                <input type="text" placeholder="Edit description here" value={todoDescription}
+                    onChange={(e) => {
+                        setTodoDescription(e.target.value);
+                        updateMyPresence({ isTyping: true });
+                    }}
+                    onBlur={() => updateMyPresence({ isTyping: false })}
+                />
+                <Button 
+                onClick={()=>{
+                    modifyTodoDescription(index, todoDescription)
+                }}
+                variant="contained">Update</Button>
                 <Typography>
-                    Pull request: https://github.com/lorem-ipsum123
+                    {description}
                 </Typography>
                 </CardContent>
             </Collapse>
+            <CardActions sx={{ justifyContent: "space-between" }} disableSpacing>
+                <IconButton>
+                    <DeleteIcon />
+                </IconButton>
+                <Button color='error' variant="contained" 
+                    onClick={()=>{
+                        deleteTodo(index)
+                    }}
+                >Delete this Todo</Button>
+            </CardActions>
         </Card>
     )
 }
