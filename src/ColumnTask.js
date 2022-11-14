5c406329e76f3a63f5b460e20ddaa419195f4cf8
Yuno Min <yunomin@client-10-228-42-5.tamulink.tamu.edu>
2022-11-14 09:42:56 -0600

    column view complete

--- src/ColumnTask.js
+++ src/ColumnTask.js
@@ -2,27 +2,17 @@ import React from 'react'
 import { Box } from '@mui/system';
 import { Paper, Typography } from '@mui/material';
 import TodoCard from "./TodoCard";
-import { v4 as uuidv4 } from 'uuid';
 
 const ColumnTask = ({todos, title}) => {
     return (
-        <Paper sx={{ marginRight: 2 }}>
+        <Paper sx={{ marginRight: 2, flexGrow: 0, flexShrink: 0, width: 375 }}>
             <Typography sx={{ fontFamily:'Geneva', color:'#667085', margin: 1.5, marginBottom: 0 }}>
                 {title.toUpperCase()}
             </Typography>
             {todos && 
                 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                     {todos.map((todo, index) => {
-                        let color;
-                        if (todo.deadline) {
-                            const deadline = new Date(todo.deadline);
-                                if (deadline < new Date()){
-                                    color = 'red'
-                                }
-                        }
-                        return (
-                            <TodoCard todo={todo} index={index}/>
-                        );
+                        return (<TodoCard todo={todo} index={index}/>)
                     })}
                 </Box>
             }    
